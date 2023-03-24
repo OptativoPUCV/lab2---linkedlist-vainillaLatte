cd9337ddcebccfa929b33ce257653990d57863fc
Replit user <>
2023-03-24 00:19:49 +0000

    new try

--- list.c
+++ list.c
@@ -119,11 +119,51 @@ void pushCurrent(List * list, void * data) {
 }
 
 void * popFront(List * list) {
-  list->current = list->head;
-  list->head->next=NULL;
-  list->current++;
+  void *popCurrent(List *list) { 
+  
+  if(list->current == NULL) return NULL;
+  
+  void* datoEliminado = list->current->data;
+  
+  
+  if(list->current == list->head)
+  {
     
-    return (list->head->data);
+    list->head = list->head->next;
+
+    if(list->head->prev != NULL)
+    {
+      list->head->prev = NULL;
+    }
+    
+  }
+  else
+  {
+    list->current->prev->next = list->current->next;
+    
+  }
+  
+  if(list->current == list->tail)
+  {
+    
+    list->tail = list->current->prev;
+    list->tail->next = NULL;
+  }
+  else
+  {
+      
+    list->current->next->prev = list->current->prev;
+    
+  }
+  
+  Node *nodoAux = list->current->next;
+  
+  free(list->current);
+
+  list->current = nodoAux;
+
+  return datoEliminado;
+}
 }
 
 void * popBack(List * list) {
